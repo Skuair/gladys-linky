a111da09f8c567b52174210423da59fd5d4ad062
Skuair <49756503+Skuair@users.noreply.github.com>
2020-11-08 16:58:17 +0100

    Firt commit with new Enedis API

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-module.exports = function(sails) {
-    
-    //var setup = require('./lib/setup.js');
-    //var exec = require('./lib/exec.js');
-    var init = require('./lib/linky.init.js');
-    var install = require('./lib/linky.install.js');
-	//var initialize = require('./lib/linky.initialize.js');
-	var getParamsClient = require('./lib/linky.getParamsClient.js');
-	var getAPIConsomDay = require('./lib/linky.getAPIConsomDay.js');
-	var getAPIConsomMonth = require('./lib/linky.getAPIConsomMonth.js');
-	var getAPIConsomYear = require('./lib/linky.getAPIConsomYear.js');
+module.exports = function (sails) {
+  var init = require("./lib/linky.init.js");
+  var install = require("./lib/linky.install.js");
+  var getParamsClient = require("./lib/linky.getParamsClient.js");
+  var getAPIConsomDay = require("./lib/linky.getAPIConsomDay.js");
+  var getAPIMaxLoadDay = require("./lib/linky.getAPIMaxLoadDay.js");
 
-	
+  gladys.on("ready", function () {
+    init().catch(sails.log.warn);
+  });
 
-
-    //var uninstall = require('./lib/uninstall.js');
-
-    gladys.on('ready', function(){
-        init().catch(sails.log.warn);
-    });
-
-    return {
-		//setup: setup,
-		install: install,
-		//uninstall: uninstall,
-		//exec: exec,
-		//initialize: initialize,
-		getParamsClient: getParamsClient,
-		getAPIConsomDay: getAPIConsomDay,
-		getAPIConsomMonth: getAPIConsomMonth,
-		getAPIConsomYear: getAPIConsomYear
-
-    };
+  return {
+    install: install,
+    getParamsClient: getParamsClient,
+    getAPIConsomDay: getAPIConsomDay,
+    getAPIMaxLoadDay: getAPIMaxLoadDay,
+  };
 };
